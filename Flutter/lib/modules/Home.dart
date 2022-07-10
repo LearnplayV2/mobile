0ec74b06ec5ef586dd8a0ae418650001a4b83b00
drelocatelli <you@example.com>
2022-07-10 12:24:42 -0300

    valueNotifier

--- Flutter/lib/modules/Home.dart
+++ Flutter/lib/modules/Home.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:learnplay/components/appBar.dart';
 import 'package:learnplay/components/basicWidgets.dart';
 import 'package:learnplay/config.dart';
+import 'package:learnplay/controller/home_controller.dart';
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -12,6 +13,18 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  final controller = HomeController();
+
+  int _counter() => controller.counter;
+
+  @override
+  void initState() {
+    super.initState();
+    controller.addListener(() {
+      setState(() { });
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -28,30 +41,31 @@ class _HomeState extends State<Home> {
             Padding(
               padding: const EdgeInsets.symmetric(vertical: 15),
               child: Align(
-                alignment: Alignment.topLeft,
-                child: Text("Esqueci minha senha", style: TextStyle(color: MainTheme.linkPrimary))
-              ),
+                  alignment: Alignment.topLeft,
+                  child: Text("Esqueci minha senha",
+                      style: TextStyle(color: MainTheme.linkPrimary))),
             ),
             SizedBox(
               height: 20,
             ),
             SizedBox(
-              width: double.infinity,
+                width: double.infinity,
                 child: WidgetList.Button(
-              onPressed: () {},
-              text: "Entrar",
-            )),
+                  onPressed: () => controller.increment(),
+                  text: "Entrar",
+                )),
             Padding(
               padding: const EdgeInsets.symmetric(vertical: 15),
               child: Align(
-                alignment: Alignment.topLeft,
-                child: Row(
-                  children: [
-                    MainTheme.normalText("Não tem uma conta? "),
-                    Text("Cadastre-se", style: TextStyle(color: MainTheme.linkPrimary))
-                  ],
-                )
-              ),
+                  alignment: Alignment.topLeft,
+                  child: Row(
+                    children: [
+                      MainTheme.normalText("Não tem uma conta? "),
+                      Text("Cadastre-se",
+                          style: TextStyle(color: MainTheme.linkPrimary)),
+                      Text("${_counter()}")
+                    ],
+                  )),
             ),
           ],
         ),
