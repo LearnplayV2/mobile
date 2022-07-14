f0d99a7d78b0f489bde858a40f82b25b7e60c8e7
drelocatelli <you@example.com>
2022-07-13 21:57:58 -0300

    view profile member

--- Flutter/lib/modules/dashboard/pages/members.dart
+++ Flutter/lib/modules/dashboard/pages/members.dart
@@ -70,7 +70,7 @@ class _DashboardMembersState extends State<DashboardMembers> {
                           width: 100,
                           height: 100,
                           child: Image.network(UserService.getProfilePicture(
-                              uuid: _members![index].uuid!))),
+                              uuid: "${_members![index].uuid!}?_=${DateTime.now().millisecondsSinceEpoch}"))),
                       SizedBox(width: 15),
                       Column(
                         crossAxisAlignment: CrossAxisAlignment.start,
