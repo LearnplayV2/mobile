111e938a79d2ad5c50772430debd25801a10da4d
drelocatelli <you@example.com>
2022-08-14 16:30:00 -0300

    init create group

--- Flutter/lib/modules/dashboard/pages/groups/new.dart
+++ Flutter/lib/modules/dashboard/pages/groups/new.dart
@@ -14,76 +14,66 @@ class NewGroup extends StatefulWidget {
 }
 
 class _NewGroupState extends State<NewGroup> {
-
   static final _formKey = GlobalKey<FormState>(debugLabel: 'new_group');
-  
+
   @override
   Widget build(BuildContext context) {
     return DashboardBar(
-      child: WidgetList.DisplayCenter(
-        context, 
-        children: [
-          MainTheme.h1("Criar novo grupo", color: MainTheme.accent),
-          SizedBox(height: 20),
-          _buildGroupForm()
-        ]
-      )
-    );
+        child: WidgetList.DisplayCenter(context, children: [
+      MainTheme.h1("Criar novo grupo", color: MainTheme.accent),
+      SizedBox(height: 20),
+      _buildGroupForm()
+    ]));
   }
 
   _buildGroupForm() {
     final _titleController = TextEditingController();
     final _visibilityController = TextEditingController(text: 'PUBLIC');
 
-    return Form(
-      key: _formKey,
-      child: Column(
-        children: [
-          WidgetList.Input(
-            controller: _titleController,
-            hintText: 'Nome do grupo',
-            textInputAction: TextInputAction.next,
-          ),
-          DropDownFormField(
-            filled: false,
-            value: _visibilityController.text,
-            titleText: "Visibilidade",
-            hintText: "Visibilidade",
-            onChanged: (value) {
-              _visibilityController.text = value;
-              setState(() { });
-            },
-            dataSource: [
-              {
-                "display": "Público",
-                "value": "PUBLIC"
-              },
-              {
-                "display": "Privado",
-                "value": "PRIVATE"
-              },
+    return StatefulBuilder(
+      builder: (ctx, setState) {
+        return Form(
+          key: _formKey,
+          child: Column(
+            children: [
+              WidgetList.Input(
+                controller: _titleController,
+                hintText: 'Nome do grupo',
+                textInputAction: TextInputAction.next,
+              ),
+              DropDownFormField(
+                filled: false,
+                value: _visibilityController.text,
+                titleText: "Visibilidade",
+                hintText: "Visibilidade",
+                onChanged: (value) {
+                  _visibilityController.text = value;
+                  setState(() {});
+                },
+                dataSource: const [
+                  {"display": "Público", "value": "PUBLIC"},
+                  {"display": "Privado", "value": "PRIVATE"},
+                ],
+                textField: 'display',
+                valueField: 'value',
+              ),
+              TextField(
+                decoration: InputDecoration(
+                    label: Text("Descrição do grupo"),
+                    labelStyle: TextStyle(color: Colors.white60, fontSize: 14),
+                    fillColor: MainTheme.light,
+                    filled: true),
+                keyboardType: TextInputType.multiline,
+                textInputAction: TextInputAction.newline,
+                minLines: 3,
+                maxLines: 8,
+              ),
+              SizedBox(height: 18),
+              ElevatedButton(onPressed: () {}, child: Text("Criar grupo"))
             ],
-            textField: 'display',
-            valueField: 'value',
           ),
-          TextField(
-            decoration: InputDecoration(
-              label: Text("Descrição do grupo"),
-              labelStyle: TextStyle(color: Colors.white60, fontSize: 14),
-              fillColor: MainTheme.light,
-              filled: true
-            ),
-            keyboardType: TextInputType.multiline,
-            textInputAction: TextInputAction.newline,
-            minLines: 3,
-            maxLines: 8,
-          ),
-          SizedBox(height: 18),
-          ElevatedButton(onPressed: () {}, child: Text("Criar grupo"))
-        ],
-      ),
+        );
+      },
     );
-    
   }
-  
 }
